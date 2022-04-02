59b7cbeafe8207604d8971f2a70b60b2e97008a1
gustavo2892 <gustavo2892@gmail.com>
2022-04-02 14:47:10 -0300

    Config react-hook-form and yup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,41 @@
-import { Box, Button, Stack, Typography } from "@mui/material";
-import type { NextPage } from "next";
+import { Box, Button, Stack, Typography } from '@mui/material';
+import type { NextPage } from 'next';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+/*
+* Observações
+* Box - DIV
+* Stack - DIV com flex-box
+*
+*/
+
+interface ILoginForm {
+  email: string;
+  password: string;
+}
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Informe um email válido')
+    .required('O email é obrigatório.'),
+  password: yup
+    .string()
+    .min(6, 'A senha deve possuir no mínimo 6 caracteres.')
+    .required('A senha é obrigatória.'),
+});
 
 const Home: NextPage = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm<ILoginForm>({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data: ILoginForm) => {
+    console.log(data);
+  };
+
   return (
     <Stack
       sx={{
@@ -12,7 +46,7 @@ const Home: NextPage = () => {
       }}
     >
       <Box>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <Stack
             spacing={2}
             sx={{
@@ -25,9 +59,27 @@ const Home: NextPage = () => {
             <Typography variant="h4" textAlign="center">
               Login RHF
             </Typography>
-            <input type="text" name="name" placeholder="Nome" />
-            <input type="password" name="password" placeholder="Senha" />
-            <Button variant="contained">Enviar</Button>
+            <input
+              type="text"
+              placeholder="Email"
+              {...register('email')}
+            />
+            {errors?.email && (
+              <Typography variant="caption" sx={{ color: "red" }}>
+                {errors?.email?.message}
+              </Typography>
+            )}
+            <input
+              type="password"
+              placeholder="Senha"
+              {...register('password')}
+            />
+            {errors?.password && (
+              <Typography variant="caption" sx={{ color: "red" }}>
+                {errors?.password?.message}
+              </Typography>
+            )}
+            <Button variant="contained" type="submit">Enviar</Button>
           </Stack>
         </form>
       </Box>
